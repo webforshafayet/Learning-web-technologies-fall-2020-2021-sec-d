8d6528a334461bd0bc4166cb3837fade76de95e2
webforshafayet <shohan123boss@gmail.com>
2020-11-11 08:26:53 +0600

    commit

---
+++ LAB_TASK_4.1_SESSION/public_home.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+	<title>HOME</title>
+</head>
+<body>
+
+	<table width="900px" border="1" align="center">
+		<tr colspan="3">
+			<td width="100px">
+				<img src="pic.png" width="100px" height="100px" ></td>
+				<td align="right">
+				<a href="home_page.php"><b>Home</b></a> | 
+				<a href="login.php"><b>Login</b></a> | 
+				<a href="registration.php"><b>Registration</b></a> 
+			</td>
+		 
+		 </tr>
+		<tr height="200px">
+			<td colspan="3" align="left">
+				<b>Welcome to xCompany</b>
+			</td>
+		</tr>		
+		<tr height="80px">
+			<td colspan="3" align="center">
+				copyright@2020
+			</td>
+		</tr>
+	 </table>
+	 
+ </body>
+</html>
