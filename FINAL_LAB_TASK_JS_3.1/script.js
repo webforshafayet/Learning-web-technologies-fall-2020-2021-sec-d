b7b760b7f56f5e39b7e0ce847ddb370bedb3fc28
webforshafayet <shohan123boss@gmail.com>
2020-12-07 09:36:22 +0600

    email js commit

---
+++ FINAL_LAB_TASK_JS_3.1/script.js
@@ -0,0 +1,41 @@
+"use strict"
+
+
+//alert('test');
+//console.log('testing...');
+
+//document.write('this is JS example');
+
+//document.getElementById('head').innerHTML = 'this is js example';
+//obj.innerHTML = "test";
+
+// name
+let student = ['alamin', '123', 'CS'];
+
+function f1(){
+	let data= document.getElementById('name').value;
+	if(data == ''){
+		document.getElementById('head').innerHTML = 'invalid data';		
+	}else{
+        document.getElementById('head').innerHTML = data;
+        
+    }
+    
+ 
+//emailllll  
+
+
+}
+
+function f2(){
+	let data= document.getElementById('name').value;
+	if(data == ''){
+		document.getElementById('head').innerHTML = 'invalid data';		
+	}else{
+        document.getElementById('head').innerHTML = data;
+        
+    }
+    
+ 
+
+}
